104f195144cba28156c699683d3f59a989d87c04
Benjamin Poon <benjamintpoon@gmail.com>
2018-08-14 22:32:05 -0600

    Fetch drinks from API on initial App load

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('/api/drinks')
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error(err))
+  }
+
   render() {
     return (
       <div className="App">
